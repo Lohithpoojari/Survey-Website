2371eafe875cee86d76ed80d1d58007417bc9635
lohith poojari <lohithpoojari98@gmail.com>
2020-11-19 16:17:53 +0530

    Initial commit

---
+++ src/components/SurveyForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SurveyForm.css";
+
+function SurveyForm() {
+  return (
+    <div className="surveyForm">
+      <h2>Questions</h2>
+    </div>
+  );
+}
+
+export default SurveyForm;
